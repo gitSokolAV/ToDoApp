457f1534bf237afb8fd2aaa43fff2c87e2e8a43f
Alex Sokol <sokol.a.v.30@gmail.com>
2023-11-13 20:09:04 +0200

    [#38] Added mod struct

--- backend.h
+++ backend.h
@@ -24,4 +24,22 @@ signals:
 private:
     QString timeFromInt;
 };
+struct str_complex
+{
+private:
+    double re, im, arg, mod;
+public:
+
+    str_complex(double a_re, double a_im)
+    {
+        re = a_re, im = a_im;
+        mod = sqrt(re*re + im*im);
+        arg = atan2(im, re);
+    }
+    double get_re(){return re;}
+    double get_im(){return im;}
+    double modulo(){return mod;}
+    double argument(){return arg;}
+};
+
 #endif // BACKEND_H
